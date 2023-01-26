bd49c62c15227377d2f1bd004b574114e6e41d2d
spiderforrest <spider@spood.org>
2023-01-25 17:52:28 -0800

    yeah ok my winchecking function don't work i'll fix her tomorrow

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -15,12 +15,30 @@ export const GameProvider = ({ children }) => {
     const newBoard = [...board];
     newBoard[idx] = currentPlayer;
     setBoard(newBoard);
+    console.log(board);
+    console.log(checkForWin());
     // change turn
     if (currentPlayer === 'x') {
       setCurrentPlayer('o');
     } else {
       setCurrentPlayer('x');
     }
+  }
+
+  function checkForWin() {
+    // i wish i was more clever :(
+    // if it's str8 in a line horizontal it's 3 in a row
+    for (let i = 1; i < 9; i *= 3) {
+      if ((board[i - 1] === board[i]) === board[i + 1]) return true;
+    }
+    // if it's str8 vertical it's every 3rd
+    for (let i = 0; i < 3; i++) {
+      if ((board[i] === board[i + 3]) === board[i + 6]) return true;
+    }
+    // if it's angle it's every 4th or 2nd
+    if ((board[0] === board[4]) === board[8]) return true;
+    if ((board[2] === board[4]) === board[6]) return true;
+    return false;
   }
 
   return (
